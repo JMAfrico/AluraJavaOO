73fa5d2b53be1adac2bf05675fd7f32536f07885
= <=>
2021-10-05 11:00:45 -0300

    Criando e invocando métodos sacar e transferir

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -2,14 +2,60 @@ package bytebank;
 
 public class Conta {
 
-	Double saldo = 0.0;
+	double saldo;
 	Integer conta;
 	Integer agencia;
 	String titular;
-	
+	String msg;
+
 	public double deposita(double valor) {
 		this.saldo += valor;
 		return valor;
 	}
-	
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public double mostrarSaldo() {
+		return this.saldo;
+	}
+
+//	public boolean transfere(double valor, Conta destino) {
+//		if(this.saldo >= valor) {
+//			this.saca(valor);
+//			destino.deposita(valor);
+//			return true;
+//		}
+//		else {
+//			return false;
+//		}
+//	}
+
+	private boolean calculoTranferencia(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saca(valor);
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public String transfere(double valor, Conta destino) {
+		if (calculoTranferencia(valor, destino)) {
+			this.msg = "Transferência realizada com sucesso. Saldo Atual: " + this.mostrarSaldo();
+			return msg;
+		} else {
+			this.msg = "Saldo insuficiênte. Saldo Atual: " + this.mostrarSaldo();
+			return msg;
+		}
+
+	}
+
 }
--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -5,10 +5,28 @@ public class TestaMetodo {
 	public static void main(String[] args) {
 
 		Conta primeiraConta = new Conta();
+		Conta segundaConta = new Conta();
+
+		// DEPOSITA
 		primeiraConta.deposita(100);
-		System.out.println(primeiraConta.saldo);
+		System.out.println(primeiraConta.mostrarSaldo());
 		primeiraConta.deposita(20);
-		System.out.println(primeiraConta.saldo);
+		System.out.println(primeiraConta.mostrarSaldo());
+
+		// SACA
+		primeiraConta.saca(20);
+		System.out.println(primeiraConta.mostrarSaldo());
+		primeiraConta.saca(40);
+		System.out.println(primeiraConta.mostrarSaldo());
+
+		// TRANSFERE COM BOOLEAN E MENSAGEM
+		primeiraConta.transfere(45, segundaConta);
+		System.out.println(primeiraConta.msg);
+		System.out.println(segundaConta.mostrarSaldo());
+
+		primeiraConta.transfere(5, segundaConta);
+		System.out.println(primeiraConta.mostrarSaldo());
+		System.out.println(segundaConta.mostrarSaldo());
 
 	}
 
